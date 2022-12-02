659eefa6803c794cfba31912d54e9af04d7eb632
Ceci <ceci.fz87@gmail.com>
2022-12-02 11:07:35 -0300

    Adviency DIA2 - Agrego gorro navideño y regalo hecho con css

---
+++ adviency_dia2/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <div className='gorro'></div>
+      <h1>Regalos</h1>
+      <ul>
+        <li>Gafas de sol 🕶️ </li>
+        <li>Ojotas 🩴</li>
+        <li>Salvavidas de dona 🍩</li>
+        <li>Libro 4 de Canción de hielo y fuego ❄️🔥</li>
+      </ul>
+      <div className='regalo'><span className='lunar'></span></div>
+    </div>
+  )
+}
+
+export default App
